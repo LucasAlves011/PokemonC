18fa597cbfecbd480601b2204637781646426705
Lucas Alves <lucas.nascimento.al16@gmail.com>
2021-10-31 03:41:54 -0300

    Renomear a imagem para um nome descente

--- main.c
+++ main.c
@@ -5,40 +5,67 @@
 //    char nome[10];
 //    int idade;
 //}aluno;
+bool andarW(int ash[]);
+void andarA();
+void andarS();
+void andarD();
+bool checar();
 
-void imprimir(char para[10][12]){
-    for (int i = 0; i < 10; ++i) {
+void imprimir(char para[12][10]){
+    int n = 0;
+    bool entrar = true;
+    bool condicao = true;
+    for (int i = 0; i < 12; ++i) {
         if(i == 0){
+            if(entrar){
+                printf("   ");
+                entrar = false;
+            }
             for (int j = 0; j < 10; ++j) {
                 printf("%d ",j);
             }
             printf("\n");
         }
-        for (int j = 0; j < 12; ++j) {
+
+        for (int j = 0; j < 10; ++j) {
+            if(j == 0){
+                printf("%d ",n++);
+            }
+            if(i <= 9 && condicao == true) {
+                printf(" ");
+                condicao = false;
+            }
             printf("%c ",para[i][j]);
         }
         printf("\n");
+        condicao = true;
     }
 }
 
+//inicializar valores com _
+char mapa[12][10];
 
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "EndlessLoop"
 int main() {
-
-    char mapa[10][12];
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 12; ++j) {
+    for (int i = 0; i < 12; ++i) {
+        for (int j = 0; j < 10; ++j) {
             mapa[i][j] = '_';
         }
     }
-
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 12; ++j) {
+    int ash[2] = {0,0};
+    //adicionar obstáculos e posição do Ash
+    for (int i = 0; i < 12; ++i) {
+        for (int j = 0; j < 10; ++j) {
             switch (j) {
                 case 0:
-                    if( i == 5 || i == 6 || (i > 7 && i < 11))
-                        mapa[i][j] = 'X';
+                    if(i == 0){
+                        mapa[i][j] = 'A';
+                    }
+                    else if( i == 5 || i == 6 || (i > 7 && i <= 11))
+                        if( i == 11) {
+                            mapa[i][j] = 'E';
+                        } else {
+                            mapa[i][j] = 'X';
+                        }
                     break;
                 case 1:
                     if((i > 0 && i < 4) || i == 5 || i == 6 || i == 8 || i == 10)
@@ -83,20 +110,23 @@ int main() {
     }
 
     imprimir(mapa);
-//    bool continuar = true;
-//    int ashe[2] = {0,0};
-//
-//
-//    //iniciar lop do jogo
-//    do{
-//
-//
-//
-//
-//
-//
-//    }while (continuar);
+
+
+
+
 
     return 0;
 }
-#pragma clang diagnostic pop
+//MAPA[][] ASH[0,0]
+bool andarW(int ash[]){
+    if((mapa[ash[0] - 1]) > 0 ){
+        if(mapa[ash[0]-1][ash[1]] == '_'){
+            ash[0] -= 1;
+            return true;
+        } else if(mapa[ash[0]-1][ash[1]] == 'X'){
+            return false;
+        }
+    }
+    return false;
+}
+
