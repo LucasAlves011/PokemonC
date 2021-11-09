b7bb869be542d46a31207d14a3ce516dce388e7a
LucasAlves011 <lucas.nascimento.al16@gmail.com>
2021-11-09 13:37:20 -0300

    adicionando dois arquivos e saindo da master

---
+++ functions.c
@@ -0,0 +1,4 @@
+//
+// Created by lucas on 09/11/2021.
+//
+
---
+++ functions.h
@@ -0,0 +1,8 @@
+//
+// Created by lucas on 09/11/2021.
+//
+
+#ifndef POKEMONC_FUNCTIONS_H
+#define POKEMONC_FUNCTIONS_H
+
+#endif //POKEMONC_FUNCTIONS_H
